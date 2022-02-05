174b7bd71b6cd4a244dab4b248ee7a46cda3fdc1
mohamedvall ibrahim <forvall.me@gmail.com>
2022-02-05 04:32:06 -1200

    add sales reports

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :orders_products
+  has_many :orders_products, dependent: :destroy
   has_many :products, through: :orders_products
+
+  def self.total_count_last_24h
+    where('created_at > ?', 24.hours.ago).count
+  end
+
+  def self.total_count_last_30d
+    where('created_at > ?', 30.days.ago).count
+  end
 end
--- app/models/orders_product.rb
+++ app/models/orders_product.rb
@@ -1,4 +1,20 @@
 class OrdersProduct < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def self.total_last_24h
+    select('SUM(products.price * orders_products.qt) as total').where('orders_products.created_at > ?', 24.hours.ago).joins(:product).to_a.first&.total || 0
+  end
+
+  def self.total_last_30d
+    select('SUM(products.price * orders_products.qt) as total').where('orders_products.created_at > ?', 30.days.ago).joins(:product).to_a.first&.total || 0
+  end
+
+  def self.total_count_last_24h
+    select('SUM(qt) as total').where('created_at > ?', 24.hours.ago).to_a.first&.total || 0
+  end
+
+  def self.total_count_last_30d
+    select('SUM(qt) as total').where('created_at > ?', 30.days.ago).to_a.first&.total || 0
+  end
 end
